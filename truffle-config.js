9f721da37c2431a43cebd8c6301d8a422677eb46
Ikey Benzaken <ikey.benz@gmail.com>
2019-06-23 18:50:54 -0700

    Added FuzzyCoin contract and removed useless boilerplate contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,28 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = "KbQuP7xkP1ZYNhJkUOXF"; // Either use this key or get yours at https://infura.io/signup. It's free.
+var mnemonic = process.env.METAMASK_MNEMONIC;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      gas: 6500000,
+      network_id: "5777"
+    },
+    ropsten:  {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
+      network_id: 3,
+      gas: 4500000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    },
+    version: "^0.5"
+  }
 };
